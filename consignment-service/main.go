03d079626037c2ed9519c3559266835ac7367101
chenyu <chenyu@2xi.com>
2019-09-02 16:50:21 +0800

    完成微服务第二版本,实现货轮检测

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	// 导如 protoc 自动生成的包
 	pb "github.com/justcy/shippy/consignment-service/proto/consignment"
+	vesselPb "github.com/justcy/shippy/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
 	"log"
 )
@@ -35,6 +36,8 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 //
 type service struct {
 	repo Repository
+	// consignment-service 作为客户端调用 vessel-service 的函数
+	vesselClient vesselPb.VesselServiceClient
 }
 
 //
@@ -43,12 +46,31 @@ type service struct {
 //
 // 托运新的货物
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, resp *pb.Response) error {
-	// 接收承运的货物
+	// Here we call a client instance of our vessel service with our consignment weight,
+	// and the amount of containers as the capacity value
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselPb.Specification{
+		MaxWeight: req.Weight,
+		Capacity: int32(len(req.Containers)),
+	})
+	if err != nil {
+		return err
+	}
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
+
+	// Save our consignment
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return err
 	}
-	resp = &pb.Response{Created: true, Consignment: consignment}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	resp.Created = true
+	resp.Consignment = consignment
 	return nil
 }
 
@@ -64,10 +86,11 @@ func main() {
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
 		)
+	vesselClient := vesselPb.NewVesselServiceClient("go.micro.srv.vessel", server.Client())
 	// 解析命令行参数
 	server.Init()
 	repo := Repository{}
-	pb.RegisterShippingServiceHandler(server.Server(),&service{repo})
+	pb.RegisterShippingServiceHandler(server.Server(),&service{repo,vesselClient})
 
 	if err := server.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
